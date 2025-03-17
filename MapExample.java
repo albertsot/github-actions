1d29eaf59de10fa1d8714c6a2d38830cfa4899b5
albertsot <92120874+albertsot@users.noreply.github.com>
2025-03-16 22:58:29 -0500

    Create MapExample ft changes.java

---
+++ MapExample.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class MapExample {
+    public static void main(String[] args) {
+
+    
+        List<String> frases = List.of("Hola mundo", "Java streams", "Hola Java");
+
+        List<String> fraseResultados=frases.stream()
+                .flatMap(frase-> Arrays.stream(frase.split(" ")))
+                .distinct()
+                .toList();
+        System.out.println("Resultado esperado: "+fraseResultados);
+
+    
+System.out.println("Hola, este es un mensaje");
+
+    }
+}
